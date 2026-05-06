2ba0b01a8aed1c86a1badf51ea3bb5666a10a194
Rizuki Takahashi <r0810.takahashi@gmail.com>
2026-05-07 08:54:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def add_entry(lang, mins):
     # Define gnome areas to avoid (percentage of the background)
     gnome_zones = [
         {'x': [10, 30], 'y': [75, 100]},  # Left walking gnome
-        {'x': [55, 85], 'y': [65, 90]},  # Right farming gnomes
+        {'x': [47, 85], 'y': [65, 92]},  # Right farming gnomes
     ]
     
     # Distance required between emojis to prevent overlapping
